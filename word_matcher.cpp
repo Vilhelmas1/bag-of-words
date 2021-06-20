34e9a105e3da51eb4f6672ac053661ea19de078f
Vilhelmas1 <judzinskis@gmail.com>
2021-06-20 21:16:47 +0300

    updates word_matcher to work with thread

--- word_matcher.cpp
+++ word_matcher.cpp
@@ -12,6 +12,32 @@
 using namespace std;
 
 
+std::map<std::string, int> read_dictionary_file(){
+  const char *fileName="example.txt";
+  ifstream paramFile;
+  paramFile.open(fileName);
+  string key;
+  int value;
+  std::map<std::string, int> mapOfWordCount;
+  while ( paramFile >> key >> value ) {
+    mapOfWordCount.insert(pair<string, int>(key, value)); // input them into the map 
+  }
+return mapOfWordCount;
+}
+
+void find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence)
+{     
+  for (std::pair<std::string, int> element : mapOfWordCount)
+  {
+    int count = element.second;
+    if (sentence.find( element.first) != std::string::npos){
+      cout << element.first <<endl;
+    }
+  }
+}
+
+
+
 struct tasks {
 
   // the mutex, condition variable and deque form a single
@@ -19,28 +45,19 @@ struct tasks {
   std::mutex m;
   std::condition_variable v;
   // note that a packaged_task<void> can store a packaged_task<R>:
-  std::deque<std::packaged_task<void()>> work;
+  std::deque<std::string> work;
 
   // this holds futures representing the worker threads being done:
   std::vector<std::future<void>> finished;
 
-  // queue( lambda ) will enqueue the lambda into the tasks for the threads
-  // to use.  A future of the type the lambda returns is given to let you get
-  // the result out.
-  template<class F, class R=std::result_of_t<F&()>>
-  std::future<R> queue(F&& f) {
-    // wrap the function object into a packaged task, splitting
-    // execution from the return value:
-    std::packaged_task<R()> p(std::forward<F>(f));
-
-    auto r=p.get_future(); // get the return value before we hand off the task
+  void queue(string f) {
     {
       std::unique_lock<std::mutex> l(m);
-      work.emplace_back(std::move(p)); // store the task<R()> as a task<void()>
+      work.emplace_back(std::move(f)); // store the task<R()> as a task<void()>
     }
     v.notify_one(); // wake a thread to work on the task
 
-    return r; // return the future result of the task
+    // return r; // return the future result of the task
   }
 
   // start N threads in the thread pool.
@@ -84,9 +101,10 @@ struct tasks {
 private:
   // the work that a worker thread does:
   void thread_task() {
+    std::map<std::string, int> mapOfWordCount = read_dictionary_file();
     while(true){
       // pop a task off the queue:
-      std::packaged_task<void()> f;
+      std::string f;
       {
         // usual thread-safe queue code:
         std::unique_lock<std::mutex> l(m);
@@ -94,44 +112,12 @@ private:
           v.wait(l,[&]{return !work.empty();});
         }
         f = std::move(work.front());
+        find_all_mathces(mapOfWordCount, f);
         work.pop_front();
       }
-      // if the task is invalid, it means we are asked to abort:
-      if (!f.valid()) return;
-      // otherwise, run the task:
-      f();
-
     }
   }
 };
-std::map<std::string, int> read_json_file(){
-  const char *fileName="example.txt";
-  ifstream paramFile;
-  paramFile.open(fileName);
-  string key;
-  int value;
-  std::map<std::string, int> mapOfWordCount;
-  // cout << value ;
-  while ( paramFile >> key >> value ) {
-    mapOfWordCount.insert(pair<string, int>(key, value)); // input them into the map 
-  }
-return mapOfWordCount;
-}
-
-void find_all_mathces()
-{
-     
-  std::map<std::string, int> mapOfWordCount = read_json_file();
-  string sentence = "Need new floor tiles and roof work"; // Taskai neveikia
-
-  for (std::pair<std::string, int> element : mapOfWordCount)
-  {
-    int count = element.second;
-    if (sentence.find( element.first) != std::string::npos){
-      cout << element.first <<endl;
-    }
-  }
-}
 
 
 int main()
@@ -139,10 +125,11 @@ int main()
 
   tasks test;
   test.start(15);
+  string sentence = "Need new floor tiles and roof work"; 
 
   for (int i = 0; i < 100; i++)
   {
-    test.queue(find_all_mathces);
+    test.queue(sentence);
 
   }
   return 0;
