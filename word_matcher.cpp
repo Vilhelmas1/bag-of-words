86639168b4c1b6acf0887e849bdc8058dcf39309
Erik Dubois <pauliusdapkus@gmail.com>
2021-06-21 16:15:04 +0300

    structure fix

--- word_matcher.cpp
+++ word_matcher.cpp
@@ -12,20 +12,38 @@
 using namespace std;
 
 
-std::map<std::string, int> read_dictionary_file(){
-  const char *fileName="example.txt";
-  ifstream paramFile;
-  paramFile.open(fileName);
-  string key;
-  int value;
-  std::map<std::string, int> mapOfWordCount;
-  while ( paramFile >> key >> value ) {
-    mapOfWordCount.insert(pair<string, int>(key, value)); // input them into the map 
-  }
-return mapOfWordCount;
-}
+class TextOperation {
+  public:
+    std::map<std::string, int> read_dictionary_file();
+    void find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence);
+    void queue(string f);
+    void start(std::size_t N=1);
+    void abort();
+    void cancel_pending();
+    void finish(); // finish enques a "stop the thread" message for every thread, then waits for them:
+    //~TextOperation();
 
-void find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence)
+    // the mutex, condition variable and deque form a single
+    // thread-safe triggered queue of tasks:
+    std::mutex m;
+    std::condition_variable v;
+    // note that a packaged_task<void> can store a packaged_task<R>:
+    std::deque<std::string> work;
+
+    // this holds futures representing the worker threads being done:
+    std::vector<std::future<void>> finished;
+
+  private:
+    void thread_task(); // the work that a worker thread does:
+};
+
+
+/**
+ *  @brief:
+ *  @note:
+ *  @return:
+ */
+void TextOperation::find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence)
 {     
   for (std::pair<std::string, int> element : mapOfWordCount)
   {
@@ -36,72 +54,83 @@ void find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence
   }
 }
 
-
-
-struct tasks {
-
-  // the mutex, condition variable and deque form a single
-  // thread-safe triggered queue of tasks:
-  std::mutex m;
-  std::condition_variable v;
-  // note that a packaged_task<void> can store a packaged_task<R>:
-  std::deque<std::string> work;
-
-  // this holds futures representing the worker threads being done:
-  std::vector<std::future<void>> finished;
-
-  void queue(string f) {
+/**
+ *  @brief:
+ *  @note:
+ *  @return:
+ */
+  void TextOperation::queue(string f) {
     {
       std::unique_lock<std::mutex> l(m);
       work.emplace_back(std::move(f)); // store the task<R()> as a task<void()>
     }
     v.notify_one(); // wake a thread to work on the task
-
-    // return r; // return the future result of the task
   }
 
-  // start N threads in the thread pool.
-  void start(std::size_t N=1){
+/**
+ *  @brief:
+ *  @note: start N threads in the thread pool.
+ *  @return:
+ */
+  void TextOperation::start(std::size_t N){
     for (std::size_t i = 0; i < N; ++i)
     {
       // each thread is a std::async running this->thread_task():
-      finished.push_back(
-        std::async(
-          std::launch::async,
-          [this]{ thread_task(); }
-        )
-      );
+      finished.push_back(std::async( std::launch::async, [this]{ thread_task(); }));
     }
   }
-  // abort() cancels all non-started tasks, and tells every working thread
-  // stop running, and waits for them to finish up.
-  void abort() {
+
+/**
+ *  @brief:
+ *  @note:    abort() cancels all non-started tasks, and tells every working thread
+              stop running, and waits for them to finish up.
+ *  @return:
+ */
+  void TextOperation::abort() {
     cancel_pending();
     finish();
   }
-  // cancel_pending() merely cancels all non-started tasks:
-  void cancel_pending() {
+
+
+/**
+ *  @brief:
+ *  @note: cancel_pending() merely cancels all non-started tasks:
+ *  @return:
+ */
+  void TextOperation::cancel_pending() {
     std::unique_lock<std::mutex> l(m);
     work.clear();
   }
+
+
+/**
+ *  @brief:
+ *  @note:
+ *  @return:
+ */
   // finish enques a "stop the thread" message for every thread, then waits for them:
-  void finish() {
-    {
+  void TextOperation::finish() {
       std::unique_lock<std::mutex> l(m);
       for(auto&&unused:finished){
         work.push_back({});
       }
     }
-    v.notify_all();
+
+
+   /* v.notify_all();
     finished.clear();
   }
   ~tasks() {
-    finish();
-  }
-private:
-  // the work that a worker thread does:
-  void thread_task() {
-    std::map<std::string, int> mapOfWordCount = read_dictionary_file();
+    finish();/**/
+  
+/**
+ *  @brief:
+ *  @note:
+ *  @return:
+ */
+  void TextOperation::thread_task() {
+    TextOperation op;
+    std::map<std::string, int> mapOfWordCount = op.read_dictionary_file();
     while(true){
       // pop a task off the queue:
       std::string f;
@@ -117,13 +146,32 @@ private:
       }
     }
   }
-};
+
+/**
+ *  @brief:
+ *  @note:
+ *  @return:
+ */
+std::map<std::string, int> TextOperation::read_dictionary_file(){
+  const char *fileName="example.txt";
+  ifstream paramFile;
+  paramFile.open(fileName);
+  string key;
+  int value;
+  std::map<std::string, int> mapOfWordCount;
+
+  while ( paramFile >> key >> value ) {
+    mapOfWordCount.insert(pair<string, int>(key, value)); // input them into the map 
+  }
+
+  return mapOfWordCount;
+}
+
 
 
 int main()
 {
-
-  tasks test;
+  TextOperation test;
   test.start(15);
   string sentence = "Need new floor tiles and roof work"; 
 
@@ -133,4 +181,4 @@ int main()
 
   }
   return 0;
-}
+}/**/
