b4d4cd47152eb2476adcdf1a4c0be6b0ac465c03
Vilhelmas1 <judzinskis@gmail.com>
2021-07-13 23:33:26 +0300

    Updated word_matcher and dataset_preparator scripts.

--- word_matcher.cpp
+++ word_matcher.cpp
@@ -8,6 +8,7 @@
 #include <sstream>
 #include <fstream>
 #include <map>
+#include <algorithm>
 
 using namespace std;
 
@@ -15,11 +16,7 @@ using namespace std;
 class TextOperation {
   public:
     std::map<std::string, int> read_dictionary_file();
-    // vector find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence);
-     vector<int> start(string sentence, map<std::string, int> mapOfWordCount);
-
-    // Užkomentuoju, bet gal reikėtu async procesus deti i finished vektoriu, iš kurio veliau rezultatus issitraukti galima.
-    // TextOperation::start funkcijoje bandžiau padaryti taip bet gaunu error neiaišku ir nepaėjo to padaryti. 
+    vector<int> start(string sentence, map<std::string, int> mapOfWordCount);
     std::vector<future<vector<int>>> finished;
 };
 
@@ -35,7 +32,7 @@ vector<int> find_all_mathces(std::map<std::string, int> mapOfWordCount, string s
   {
     int count = element.second;
     if (sentence.find( element.first) != std::string::npos){
-      item_ids.push_back(count);
+      item_ids.push_back(count / 100);
     }
   }
   return item_ids;
@@ -47,11 +44,7 @@ vector<int> find_all_mathces(std::map<std::string, int> mapOfWordCount, string s
  *  @return: vector<int> object with map ids.
  */
   vector<int> TextOperation::start(string sentence, map<std::string, int> mapOfWordCount){
-    // each thread is a std::async running this->find_all_matches():
     future<vector<int>> fut = std::async(std::launch::async, find_all_mathces, mapOfWordCount, sentence);
-
-    finished.push_back(std::async(std::launch::async, find_all_mathces, mapOfWordCount, sentence));
-
     fut.wait();
     return fut.get();
   }
@@ -62,46 +55,62 @@ vector<int> find_all_mathces(std::map<std::string, int> mapOfWordCount, string s
  *  @return: Map with word in dictionary and id.
  */
 std::map<std::string, int> TextOperation::read_dictionary_file(){
-  const char *fileName="example.txt";
+  const char *fileName="word_list.txt";
   ifstream paramFile;
   paramFile.open(fileName);
   string key;
   int value;
+  int count = 0;
   std::map<std::string, int> mapOfWordCount;
 
   while ( paramFile >> key >> value ) {
-    mapOfWordCount.insert(pair<string, int>(key, value)); // input them into the map 
+    mapOfWordCount.insert(pair<string, int>(key, count)); // input them into the map 
+    count++;
   }
 
   return mapOfWordCount;
 }
 
+bool sortByVal(const pair<int, int> &a, 
+               const pair<int, int> &b) 
+{ 
+    return (a.second > b.second); 
+} 
 
+
+// Reiktu manau paskutini dalyka padaryti tai priderinti prie tavo atsiusto json, bet tam reiktu labiau pergeneruoti word_list file negu kazka kodiskai keisti.
+// Kol kas sugeneravau random klases pagal bbc dataset. ['business' 'entertainment' 'politics' 'sport' 'tech']
 int main()
 {
   TextOperation test;
-  string sentence = "Need new floor tiles and roof work"; 
+  string sentence = "Need new floor tiles and roof work, year, tv, want web"; 
+  
   map<std::string, int> mapOfWordCount = test.read_dictionary_file();
+  vector<int> unique_classes{0,1,2,3,4};
+  // ['business' 'entertainment' 'politics' 'sport' 'tech']
   vector<int> ids;
-  for (int i = 0; i < 100; i++)
-  {
-    ids = test.start(sentence, mapOfWordCount);
-    // Check if there are any return values
-    for (auto i = ids.begin(); i != ids.end(); ++i)
-    std::cout << *i << ' ';
+  vector<pair<int,int>> occurence;
 
+  for (int i = 0; i < 1; i++)
+  {
+    // callini funkcija ir gauni subgrubiu ids
+    ids = test.start(sentence, mapOfWordCount);
+
+    // Apskaciuji kiek vienos subgrupes zodziu buvo rasta ir grupes id + rastu zodziu count i vektoriu sudedi
+    for (auto i = unique_classes.begin(); i != unique_classes.end(); ++i) {
+      occurence.push_back(make_pair(*i, count(ids.begin(), ids.end(), *i)));
+    }
+
+  // Sortini pagal rastu zodziu kieki. Dazniausi pirmose vietose
+  sort(occurence.begin(), occurence.end(), sortByVal); 
+
+  // Siaip print kad paziureti ka gavai
+  cout << "The vector, sorted by value is: " << endl;
+	for (int i = 0; i < occurence.size(); i++)
+	{
+		cout << occurence[i].first << ": " << occurence[i].second << endl;
+	}
   }
 
-  // Reiktu kaip nors issitraukti iš test.finished ta future objekta. Bet viskas kas bandau man failina
-  vector<future<vector<int>>> finished = test.finished;
-  future<vector<int>> first_value = finished.size();
-
-
-
-//   for (auto i = finished.begin(); i !=  finished.end(); ++i){
-
-//     future<vector<int>> fut = finished.begin();
-//     std::cout << i << ' ';
-// }
   return 0;
-}/**/
+}
