980393ecf4716376030552028e3b0ef529d324b3
Vilhelmas1 <judzinskis@gmail.com>
2021-07-14 17:24:11 +0300

    Update word_matcher class

--- word_matcher.cpp
+++ word_matcher.cpp
@@ -14,37 +14,56 @@ using namespace std;
 
 
 class TextOperation {
+  vector<int> unique_classes{0,1,2,3,4};
+
   public:
     std::map<std::string, int> read_dictionary_file();
-    vector<int> start(string sentence, map<std::string, int> mapOfWordCount);
+    // vector<int> find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence);
+    vector<pair<int,int>> start(string sentence, map<std::string, int> mapOfWordCount);
     std::vector<future<vector<int>>> finished;
+
+    
 };
 
+bool sortByVal(const pair<int, int> &a, 
+               const pair<int, int> &b) 
+{ 
+    return (a.second > b.second); 
+} 
 /**
  *  @brief: Matcher function that checks if text has value from map
  *  @note: 
  *  @return: returns vectorized ids of words that exist in text
  */
-vector<int> find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence)
-{ 
-  vector<int> item_ids;
-  for (std::pair<std::string, int> element : mapOfWordCount)
-  {
-    int count = element.second;
-    if (sentence.find( element.first) != std::string::npos){
-      item_ids.push_back(count / 100);
+vector<pair<int,int>> find_all_mathces(std::map<std::string, int> mapOfWordCount, string sentence, vector<int> unique_classes)
+    { 
+      vector<int> item_ids;
+      vector<pair<int,int>> occurence;
+
+      for (std::pair<std::string, int> element : mapOfWordCount)
+      {
+        int count = element.second;
+        if (sentence.find( element.first) != std::string::npos){
+          item_ids.push_back(count / 100);
+        }    
+      }
+
+      for (auto i = unique_classes.begin(); i != unique_classes.end(); ++i) {
+          occurence.push_back(make_pair(*i, count(item_ids.begin(), item_ids.end(), *i)));
+        }
+
+      sort(occurence.begin(), occurence.end(), sortByVal); 
+      return occurence;
     }
-  }
-  return item_ids;
-}
+
 
 /**
  *  @brief: Main function that starts an async proccess and returns a vector with ids of words found in text.
  *  @note: start an async proccess that find map values in provided sentence.
  *  @return: vector<int> object with map ids.
  */
-  vector<int> TextOperation::start(string sentence, map<std::string, int> mapOfWordCount){
-    future<vector<int>> fut = std::async(std::launch::async, find_all_mathces, mapOfWordCount, sentence);
+vector<pair<int,int>> TextOperation::start(string sentence, map<std::string, int> mapOfWordCount){
+    future<vector<pair<int,int>>> fut = std::async(std::launch::async, find_all_mathces, mapOfWordCount, sentence, unique_classes);
     fut.wait();
     return fut.get();
   }
@@ -71,11 +90,7 @@ std::map<std::string, int> TextOperation::read_dictionary_file(){
   return mapOfWordCount;
 }
 
-bool sortByVal(const pair<int, int> &a, 
-               const pair<int, int> &b) 
-{ 
-    return (a.second > b.second); 
-} 
+
 
 
 // Reiktu manau paskutini dalyka padaryti tai priderinti prie tavo atsiusto json, bet tam reiktu labiau pergeneruoti word_list file negu kazka kodiskai keisti.
@@ -86,30 +101,22 @@ int main()
   string sentence = "Need new floor tiles and roof work, year, tv, want web"; 
   
   map<std::string, int> mapOfWordCount = test.read_dictionary_file();
-  vector<int> unique_classes{0,1,2,3,4};
   // ['business' 'entertainment' 'politics' 'sport' 'tech']
-  vector<int> ids;
-  vector<pair<int,int>> occurence;
+  vector<pair<int,int>> ids;
 
   for (int i = 0; i < 1; i++)
   {
     // callini funkcija ir gauni subgrubiu ids
     ids = test.start(sentence, mapOfWordCount);
 
-    // Apskaciuji kiek vienos subgrupes zodziu buvo rasta ir grupes id + rastu zodziu count i vektoriu sudedi
-    for (auto i = unique_classes.begin(); i != unique_classes.end(); ++i) {
-      occurence.push_back(make_pair(*i, count(ids.begin(), ids.end(), *i)));
-    }
+    // Siaip print kad paziureti ka gavai
+    cout << "The vector, sorted by value is: " << endl;
+    for (int i = 0; i < ids.size(); i++)
+      {
+        cout << ids[i].first << ": " << ids[i].second << endl;
+      }
 
-  // Sortini pagal rastu zodziu kieki. Dazniausi pirmose vietose
-  sort(occurence.begin(), occurence.end(), sortByVal); 
-
-  // Siaip print kad paziureti ka gavai
-  cout << "The vector, sorted by value is: " << endl;
-	for (int i = 0; i < occurence.size(); i++)
-	{
-		cout << occurence[i].first << ": " << occurence[i].second << endl;
-	}
+      
   }
 
   return 0;
